28a6bde27fd7339cc133d5c9fd3d59dfcb2ff34f
Alonso Donayre <alonso2295@hotmail.com>
2023-05-22 21:24:20 -0500

    Added taskgroup sequential implementation

--- dags/taskgroups.py
+++ dags/taskgroups.py
@@ -26,8 +26,18 @@ with DAG(dag_id="taskgroups",
     init_variables = BashOperator(task_id='init_variables', bash_command='sleep 3')
     processing = BashOperator(task_id='processing', bash_command='sleep 3')
     
-    with TaskGroup('task_execution') as task_execution:    
-        for i in range(1,31):
-            task = BashOperator(task_id=f'task_{i}', bash_command='sleep 6')
+    # Parallel implementation
+    # with TaskGroup('task_execution') as task_execution:    
+    #     for i in range(1,31):
+    #         task = BashOperator(task_id=f'task_{i}', bash_command='sleep 4')
+    
+    # Sequential implementation
+    with TaskGroup('task_execution') as task_execution:
+        task_1 = BashOperator(task_id='task_1', bash_command='sleep 3')     
+        task_2 = BashOperator(task_id='task_2', bash_command='sleep 3')
+        task_3 = BashOperator(task_id='task_3', bash_command='sleep 3')  
+        
+        init_variables >> task_1 >> task_2 >> task_3
+        
         
     start >> init_variables >> task_execution >> processing >> end
