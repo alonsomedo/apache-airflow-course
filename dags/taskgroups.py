3c365c032e7aa007f08a9884e67fb086087461ea
Alonso Donayre <alonso2295@hotmail.com>
2023-05-22 21:17:01 -0500

    Added taskgroup

---
+++ dags/taskgroups.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from airflow.utils.task_group import TaskGroup
+
+from datetime import datetime, timedelta
+
+# All the arguments available for default_args are the ones that belongs to the BaseOperator.
+default_args = {
+    "owner": "DSRP Class",
+    "start_date": datetime(2023,5,22),
+    "depends_on_past": True,
+    "retries": 3,
+    "retry_delay": timedelta(seconds=10)
+}
+
+with DAG(dag_id="taskgroups",
+          schedule='@daily',
+          catchup=False,
+          default_args=default_args
+          ) as dag:
+
+    start = EmptyOperator(task_id='start')
+    end = EmptyOperator(task_id='end')
+
+    init_variables = BashOperator(task_id='init_variables', bash_command='sleep 3')
+    processing = BashOperator(task_id='processing', bash_command='sleep 3')
+    
+    with TaskGroup('task_execution') as task_execution:    
+        for i in range(1,31):
+            task = BashOperator(task_id=f'task_{i}', bash_command='sleep 6')
+        
+    start >> init_variables >> task_execution >> processing >> end
